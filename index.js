8231fa3a1b69ea429f515cf9c01a457cd96e7431
Nagasai <nagasaiedapalapati@gmail.com>
2024-11-10 15:39:57 -0500

    commit files

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World! Continuous Delivery Pipeline is Working!');
+});
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
